4e0e2123d8b5e0c987b32946099486d2847d5e66
SimonWoodtli <xnasero@posteo.net>
2023-04-18 13:04:35 -0500

    ♻️ loop over files array to copy files

--- config/build/update-services/build.sh
+++ config/build/update-services/build.sh
@@ -2,11 +2,14 @@
 
 set -ouex pipefail
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+declare SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+declare -a FILES=("flatpak-user-update.timer" "flatpak-user-update.service" "10-flatpak-user-update.preset" "flatpak-system-update.timer" "flatpak-system-update.service" "10-flatpak-system-update.preset" "rpm-ostreed.conf")
 
 mkdir -p /tmp/ublue-os/rpmbuild/SOURCES
 
-cp ${SCRIPT_DIR}/{flatpak-user-update.timer,flatpak-user-update.service,10-flatpak-user-update.preset,flatpak-system-update.timer,flatpak-system-update.service,10-flatpak-system-update.preset,rpm-ostreed.conf} /tmp/ublue-os/rpmbuild/SOURCES
+for file in "${FILES[@]}"; do
+  cp "${SCRIPT_DIR}/$file" /tmp/ublue-os/rpmbuild/SOURCES
+done
 
 rpmbuild -ba \
     --define '_topdir /tmp/ublue-os/rpmbuild' \
