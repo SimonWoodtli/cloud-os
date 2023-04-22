cdba57edc1a9907cec31421f3572d50ed9f8b5c0
SimonWoodtli <xnasero@posteo.net>
2023-04-21 20:33:52 -0500

    💡 add comment

--- post-install.sh
+++ post-install.sh
@@ -8,4 +8,5 @@ systemctl enable flatpak-system-update.timer
 systemctl --global enable flatpak-user-update.timer
 
 cp /usr/share/ublue-os/ublue-os-update-services/rpm-ostreed.conf /etc/rpm-ostreed.conf
-## TODO add furter config stuff here, dconf and all that jazz
+## TODO add furter config stuff here, dconf and all that jazz, or maybe
+#it's not gonna work then add it to config/build/just and during firstboot add data
